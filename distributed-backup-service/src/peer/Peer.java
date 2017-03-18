57d3efe263ed7f5d6028950d42698cad230ffa35
Diogo Campos <d.diogocampos@gmail.com>
2017-03-18 13:52:16 +0000

    Parsing peer arguments

--- distributed-backup-service/src/peer/Peer.java
+++ distributed-backup-service/src/peer/Peer.java
@@ -1,5 +1,7 @@
 package peer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -10,23 +12,45 @@ import channels.RMIservice;
 public class Peer implements RMIservice {
 
 	private static String RMIobjName;
+	
+	private static int protocolVersion;
+	private static String senderId;
+	private static String accessPoint;
+	
+	private static InetAddress mcAddress;
+	private static int mcPort;
+	private static InetAddress mdbAddress;
+	private static int mdbPort;
+	private static InetAddress mdrAddress;
+	private static int mdrPort;
 
-	public static void main(String [] args) {
-		/*if (!validArgs(args)) {
-			System.out.println("Invalid Arguments");
+	public static void main(String [] args) throws UnknownHostException {
+		if (!validateArgs(args)) {
+			System.out.println("USAGE: <mcAddress> <mcPort> <mdbAddress> <mdbPort> <mdrAddress> <mdrPort>");
 			return;
-		}*/
+		}
 
 		startRMIservice();
 	}
 
-	public static boolean validArgs(String [] args) {
+	public static boolean validateArgs(String [] args) throws UnknownHostException {
 		// args_order -> MC MDB MDR PROTOCOL_VERSION SERVER_ID SERVICE_ACCESS_POINT
 		//                                                     ^-> ver section 5.2 remote obj name?
-		if(args.length != 6) {
-			System.out.println("");
+		if (args.length != 6) {
+			return false; 
 		}
-		return false; //TODO
+		
+		protocolVersion = Integer.parseInt(args[0]);
+		serverId = args[1];
+		accessPoint = args[2];
+		mcAddress = InetAddress.getByName(args[3]);
+		mcPort = Integer.parseInt(args[4]);
+		mdbAddress = InetAddress.getByName(args[5]);
+		mdbPort = Integer.parseInt(args[6]);
+		mdrAddress = InetAddress.getByName(args[7]);
+		mdrPort = Integer.parseInt(args[8]);
+		
+		return true;
 	}
 
 	public static void startRMIservice() {
