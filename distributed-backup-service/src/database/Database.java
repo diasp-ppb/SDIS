feef168a79f14d8f093f463d0f4a5af7a7053ddd
rodavoce <up201404178@fe.up.pt>
2017-03-21 12:24:43 +0000

    database

--- distributed-backup-service/src/database/Database.java
+++ distributed-backup-service/src/database/Database.java
@@ -1,5 +1,28 @@
 package database;
 
+import java.util.HashMap;
+
 public class Database {
 
+	HashMap<String,String> chunksInfo;
+	
+	public Database() {
+		
+		chunksInfo = new HashMap<String,String> ();
+	}
+	
+	
+	String getChunkInfo (String key){
+		return chunksInfo.get(key);
+	}
+	
+	
+	void saveChunkInfo (String key, String info){
+		chunksInfo.put(key,info);
+	}
+	
+	boolean chunkOnDB(String key){
+		return chunksInfo.containsKey(key);
+	}
+	
 }
