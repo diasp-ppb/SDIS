18a9ff912186a47c25caf356a887dcd53bc82ad6
Diogo Campos <d.diogocampos@gmail.com>
2017-03-27 22:36:00 +0100

    Added serialVersionUID to InvalidArgumentsException

--- distributed-backup-service/src/peer/InvalidArgumentsException.java
+++ distributed-backup-service/src/peer/InvalidArgumentsException.java
@@ -1,6 +1,8 @@
 package peer;
 
 public class InvalidArgumentsException extends Exception {
+	private static final long serialVersionUID = 1L;
+
 	public InvalidArgumentsException(String message){
 		super(message);
 	}
