3234a8b8704df10e97f68345369d8739bb109c02
rodavoce <up201404178@fe.up.pt>
2017-04-08 14:36:23 +0100

    random time generator

--- distributed-backup-service/src/utils/Utils.java
+++ distributed-backup-service/src/utils/Utils.java
@@ -4,6 +4,7 @@ package utils;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 public class Utils {
@@ -12,8 +13,7 @@ public class Utils {
 	private static final Pattern INTEGER = Pattern.compile("[0-9]+");
 	private static final Pattern IPV4 = Pattern.compile(".*/([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+):([0-9]+)");
 	private static final Pattern IPV6 = Pattern.compile(".*/([0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+:[0-9a-f]+(%[a-zA-Z0-9]+)?):([0-9]+)");
-	
-	
+	private static final Random randomGenerator = new Random();
 	
 	
 	
@@ -32,5 +32,9 @@ public class Utils {
 	public static final byte[] sha256(String text) throws NoSuchAlgorithmException{
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		return digest.digest(text.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	public int randomNumber(int min, int max) {
+		return randomGenerator.nextInt() % (max-min) + min;
 	}
 }
